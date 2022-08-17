ebc7c4f1ba3dd26ecf007d1d989a8f254e75458b
Janiece Eleonor <janiecee@un-n2.kzn.21-school.ru>
2022-08-18 02:35:04 +0300

    janiecee NOT FIX

--- src/prototype/app.py
+++ src/prototype/app.py
@@ -4,6 +4,9 @@ from flask import Flask, request
 # from mailer import Mailer
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 import random
 import smtplib
 
@@ -11,7 +14,7 @@ gmail_user = 'iiepe6op@gmail.com'
 gmail_password = 'aarkahldsqizguga'
 
 
-username = 'janiecee'
+# username = 'janiecee'
 postfixMail = '@student.21-school.ru'
 
 def sendEmail(gmail_user, username):
@@ -39,8 +42,9 @@ TOKEN_API = '639642745:AAHd9aIHomuZZH7-pxJPRpWAAdMjF4vHRWc'
 
 bot = Bot(token=TOKEN_API)
 dp = Dispatcher(bot)
+storage = MemoryStorage()
 
-button1 = KeyboardButton('Enter username')
+button1 = KeyboardButton('/username')
 button2 = KeyboardButton('/info')
 button5 = KeyboardButton('/poll')
 keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(button1, button2, button5)
@@ -50,6 +54,9 @@ button4 = KeyboardButton('Where are you?', request_location=True)
 keyboard2 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).row(button3, button4)
 
 app = Flask(__name__)
+
+class Form(StatesGroup):
+    username = State()
 
 @dp.message_handler(commands=["poll"])
 async def cmd_start(message: types.Message):
@@ -92,31 +99,65 @@ async def msg_with_poll(message: types.Message):
     await message.reply(f'Викторина сохранена. Общее число сохранённых викторин: {len(quizzes_database[str(message.from_user.id)])}')
 
 @dp.message_handler(commands=['start', 'help'])
-async def welcome(message: types.Message):
-    await message.reply("Privet. Kak tvoi dela?", reply_markup=keyboard1)
+async def start(message: types.Message):
+    """Conversation entrypoint"""
+    # Set state
+    await Form.username.set()
+    await message.reply("Send me your name", reply_markup=keyboard1)
 
 @dp.message_handler(commands=['info'])
 async def info(message: types.Message):
-    await message.reply('Say ok', reply_markup=keyboard2)
+    await message.reply('Contact and location', reply_markup=keyboard2)
 
 @dp.message_handler(commands=['cancel'])
 async def menu(message: types.Message):
-    await message.reply("Privet. Kak tvoi dela?", reply_markup=keyboard1)
+    await message.reply("Menu", reply_markup=keyboard1)
 
-@dp.message_handler()
-async def kb_answer(message: types.Message):
-    if message.text == 'Enter username':
-        print('name enter!')
-        if message.answer(message.text) != '' :
-            @dp.callback_query_handler(text=[{message.text}])
-            async def enterCode(call: types.CallbackQuery):
-                rndFour = sendEmail(gmail_user, username)
-                await message.answer('Enter CODE from email')
-                @dp.callback_query_handler(text=[str(rndFour)])
-                async def checkCode(call: types.CallbackQuery):
-                    if message.text == str(rndFour):
-                        await message.answer('SUCCESS!')
-                    else:
-                        await message.answer(f'CODE: {message.text} WRONG, TRY AGAIN!')
+
+# @dp.message_handler()
+# async def kb_answer(message: types.Message):
+#     if message.text == 'Enter username':
+#         print('name enter!')
+#         if message.answer(message.text) != '' :
+#             @dp.callback_query_handler(text=[{message.text}])
+#             async def enterCode(call: types.CallbackQuery):
+#                 rndFour = sendEmail(gmail_user, username)
+#                 await message.answer('Enter CODE from email')
+#                 @dp.callback_query_handler(text=[str(rndFour)])
+#                 async def checkCode(call: types.CallbackQuery):
+#                     if message.text == str(rndFour):
+#                         await message.answer('SUCCESS!')
+#                     else:
+#                         await message.answer(f'CODE: {message.text} WRONG, TRY AGAIN!')
+
+@dp.message_handler(state=Form.username)
+async def process_name(message: types.Message, state: FSMContext):
+    """Process user name"""
+    # Finish our conversation
+    username = {message.text}
+    rndFour = sendEmail(gmail_user, username)
+    await state.finish()
+    await message.reply(f"Hello, {message.text}")
+    if code == str(rndFour):
+        await message.reply("/poll")
+
+# @dp.message_handler(commands=['username'])
+# async def add_username_to_db(message: types.Message):
+#     # if message.text == 'Enter username':
+#     # message.reply_to_message is a types.Message object too
+#     try:
+#         username = message.reply_to_message.text
+#         if username: # if replied
+#             rndFour = sendEmail(gmail_user, username)
+#     except AttributeError:
+#         username = 'not replied'
+#     try:
+#         code = message.reply_to_message.text  # if replied
+#         if code == str(rndFour):
+#             await message.reply("/poll")
+#     except AttributeError:
+#         code = 'not replied'
+    # await message.answer(f'Replied message text: {username, code}')
+    # await message.answer(f'Message text: {message.text}')
 
 executor.start_polling(dp)
